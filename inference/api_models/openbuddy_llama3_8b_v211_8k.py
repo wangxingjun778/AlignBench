30553ffdf84601c013001db8c08fde24839e5a47
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-04-28 17:09:35 +0800

    update

---
+++ inference/api_models/openbuddy_llama3_8b_v211_8k.py
@@ -0,0 +1,32 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+from inference.models import api_model
+
+from llmuses.models.model_adapter import ChatGenerationModelAdapter
+
+
+class openbuddy_llama3_8b_v211_8k(api_model):
+    def __init__(self, workers=10):
+        self.temperature = 0.95
+        self.max_tokens = 1024
+        self.workers = workers
+
+        self.model_adapter = ChatGenerationModelAdapter(model_id='OpenBuddy/openbuddy-llama3-8b-v21.1-8k',
+                                                        model_revision='master',
+                                                        template_type='llama3')
+
+        super().__init__(workers)
+
+    def get_api_result(self, sample):
+        question = sample["question"]
+
+        res_d: dict = self.model_adapter.predict(question,
+                                                 infer_cfg={
+                                                     'do_sample': True,
+                                                     'max_new_tokens': 256,
+                                                     'temperature': 0.75,
+                                                            }
+                                                 )
+
+        ans: str = res_d['choices'][0]['message']['content']
+
+        return ans
